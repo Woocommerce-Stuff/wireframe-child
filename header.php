8ac7155bdae8e2d13ba4057d61172abc95ca5e82
Tada Burke <tadaburke@users.noreply.github.com>
2017-05-19 21:00:26 -0400

    Fixed site-title padding.

--- header.php
+++ header.php
@@ -49,7 +49,7 @@
 
 		<header id="masthead" class="site-header" role="banner">
 
-			<div class="site-branding">
+			<div class="site-branding container-fluid">
 
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 
